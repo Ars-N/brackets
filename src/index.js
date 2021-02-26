77ea7833c76a24ce0d4786f32b5e7f7f0f0cc4e4
ars-n <omarowarr@yandex.ru>
2021-02-26 15:22:42 +0300

    error: pathspec completed did not match any file(s) known to git

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = (str, bracketsConfig)=> {
+    let s,
+        a=()=>{s=str
+            bracketsConfig.map((a) => a.join('')).forEach(el=>{str=str.replace(el,'')})
+            return str===0?true:s===str?false:a()
+        }
+    a()
+    return  str?false:true
 }
+
